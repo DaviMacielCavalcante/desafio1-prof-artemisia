a6b14d73a3d29d6dfb997f88e16c25a3f3c9cad2
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2025-05-01 09:52:29 -0300

    :wrench: chore: Tornar o código de criação de tabelas mais seguro

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -5,37 +5,42 @@ DECLARE
 BEGIN
 	FOREACH nome IN ARRAY nomes
 	LOOP
-		EXECUTE format('
-			CREATE TABLE IF NOT EXISTS %I (
-			id SERIAL PRIMARY KEY,
-			ano INT UNIQUE NOT NULL,
-			receita_liquida NUMERIC(12,2) NOT NULL,	
-			custo_mercadorias NUMERIC(12,2) NOT NULL,	
-			subvencoes_receitas_op NUMERIC(12,2) NOT NULL,	
-			valor_bruto_producao NUMERIC(12,2) NOT NULL,	
-			consumo_intermediario_total NUMERIC(12,2) NOT NULL, 	
-			consumo_mercadorias_reposicao NUMERIC(12,2) NOT NULL,	
-			consumo_combustiveis NUMERIC(12,2) NOT NULL, 	
-			consumo_servicos_terceiros NUMERIC(12,2) NOT NULL,	
-			consumo_alugueis_imoveis NUMERIC(12,2) NOT NULL,	
-			consumo_seguros NUMERIC(12,2) NOT NULL, 	
-			consumo_comunicacao NUMERIC(12,2) NOT NULL, 
-			consumo_energia_gas_agua NUMERIC(12,2) NOT NULL,  
-			consumo_outros_custos NUMERIC(12,2) NOT NULL,
-			valor_adicionado_bruto NUMERIC(12,2) NOT NULL, 
-			gastos_pessoal_total NUMERIC(12,2) NOT NULL, 
-			gastos_salarios_remuneracoes NUMERIC(12,2) NOT NULL,
-			gastos_previdencia_social NUMERIC(12,2) NOT NULL,
-			gastos_fgts NUMERIC(12,2) NOT NULL,
-			gastos_previdencia_privada NUMERIC(12,2) NOT NULL,
-			gastos_indenizacoes_trabalhistas NUMERIC(12,2) NOT NULL,
-			gastos_beneficios_empregados NUMERIC(12,2) NOT NULL, 
-			pis_folha_pagamento NUMERIC(12,2) NOT NULL,
-			excedente_operacional_bruto NUMERIC(12,2) NOT NULL,     
-			pessoal_ocupado NUMERIC(12,2) NOT NULL, 
-			numero_empresas NUMERIC(12,2) NOT NULL
-		)
-		', nome);
+		BEGIN
+			EXECUTE format('
+				CREATE TABLE IF NOT EXISTS %I (
+				id SERIAL PRIMARY KEY,
+				ano INT UNIQUE NOT NULL,
+				receita_liquida NUMERIC(12,2) NOT NULL,	
+				custo_mercadorias NUMERIC(12,2) NOT NULL,	
+				subvencoes_receitas_op NUMERIC(12,2) NOT NULL,	
+				valor_bruto_producao NUMERIC(12,2) NOT NULL,	
+				consumo_intermediario_total NUMERIC(12,2) NOT NULL, 	
+				consumo_mercadorias_reposicao NUMERIC(12,2) NOT NULL,	
+				consumo_combustiveis NUMERIC(12,2) NOT NULL, 	
+				consumo_servicos_terceiros NUMERIC(12,2) NOT NULL,	
+				consumo_alugueis_imoveis NUMERIC(12,2) NOT NULL,	
+				consumo_seguros NUMERIC(12,2) NOT NULL, 	
+				consumo_comunicacao NUMERIC(12,2) NOT NULL, 
+				consumo_energia_gas_agua NUMERIC(12,2) NOT NULL,  
+				consumo_outros_custos NUMERIC(12,2) NOT NULL,
+				valor_adicionado_bruto NUMERIC(12,2) NOT NULL, 
+				gastos_pessoal_total NUMERIC(12,2) NOT NULL, 
+				gastos_salarios_remuneracoes NUMERIC(12,2) NOT NULL,
+				gastos_previdencia_social NUMERIC(12,2) NOT NULL,
+				gastos_fgts NUMERIC(12,2) NOT NULL,
+				gastos_previdencia_privada NUMERIC(12,2) NOT NULL,
+				gastos_indenizacoes_trabalhistas NUMERIC(12,2) NOT NULL,
+				gastos_beneficios_empregados NUMERIC(12,2) NOT NULL, 
+				pis_folha_pagamento NUMERIC(12,2) NOT NULL,
+				excedente_operacional_bruto NUMERIC(12,2) NOT NULL,     
+				pessoal_ocupado NUMERIC(12,2) NOT NULL, 
+				numero_empresas NUMERIC(12,2) NOT NULL
+			)
+			', nome);
+			 RAISE NOTICE 'Tabela % criada com sucesso.', nome;
+        EXCEPTION
+            WHEN OTHERS THEN
+                RAISE WARNING 'Erro ao criar a tabela %: %', nome, SQLERRM;
 	END LOOP;
 END;
 $$ LANGUAGE plpgsql;
